6ce3442c926dd64a99f8dd218ae504842544ae13
Smithmichaeltodd <Smithmichaeltodd@gmail.com>
2016-04-19 19:37:57 -0500

    Update Whats The Temperature.py
    Testing out multiplie print statements for a single line result

--- Whats The Temperature.py
+++ Whats The Temperature.py
@@ -6,6 +6,9 @@ def temperature():
     print("Hi, I'm the temperature converter")
     C = eval(input("Please input the temperature in Celsius: "))
     F = (9/5)*C+32
-    print("The temperature is",F,"degrees Fahrenheit outside")
+    print("The temperature is", end=" ")
+    print(F, end=" ")
+    print("degrees Fahrenheit", end=" ")
+
 
 temperature()
