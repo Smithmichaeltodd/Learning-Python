84ae8adb4fa2eda941e42a8d0123e72d0cb69169
Michael Smith <Smithmichaeltodd@gmail.com>
2016-06-28 17:18:22 -0500

    Update Whats The Temperature.py
    Updated to include temperature GUI

--- Whats The Temperature.py
+++ Whats The Temperature.py
@@ -1,14 +1,39 @@
-#Whats The Temperature.py
-#A Fahrenheit to Celsius Converter
+#GuiTempConv.py
+#Program converts Celsius to Fahernheit using a simple GUI
 #By Michael Smith
 
-def temperature():
-    print("Hi, I'm the temperature converter")
-    C = eval(input("Please input the temperature in Celsius: "))
-    F = (9/5)*C+32
-    print("The temperature is", end=" ")
-    print(F, end=" ")
-    print("degrees Fahrenheit", end=" ")
+from graphics import*
+
+def main():
+    win = GraphWin("Celsius Converter", 400, 300)
+    win.setCoords(0.0, 0.0, 3.0, 4.0)
+
+    #Draw the interface
+    Text(Point(1,3),"Celsius Temperature:").draw(win)
+    Text(Point(1,1),"Fahrenheit Temperature:").draw(win)
+    input = Entry(Point(2,3), 5)
+    input.setText("0.0")
+    input.draw(win)
+    output = Text(Point(2,1), "")
+    output.draw(win)
+    button = Text(Point(1.5,2.0), "Convert It!")
+    button.draw(win)
+    Rectangle(Point(1, 1.5), Point(2,2.5)).draw(win)
+
+    #wait for a mouse click
+    win.getMouse()
+
+    #Converter input
+    C = eval(input.getText())
+    F = 9.0/5.0*C+32
+
+    #display output and change button
+    output.setText(F)
+    button.setText("Quit")
 
 
-temperature()
+    #wait for cclick and then quit
+    win.getMouse()
+    win.close()
+
+main()
