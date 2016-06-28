abe69d5c3a8b4aed4bd98f98738737e2cae60309
Michael Smith <Smithmichaeltodd@gmail.com>
2016-06-28 16:41:19 -0500

    Update APR Chart.py
    Updated to include coordinate system

--- APR Chart.py
+++ APR Chart.py
@@ -11,17 +11,19 @@ def main():
     apr = eval(input("Enter the APR: "))
 
     #create a graphics window with labels on left edge
-    win = GraphWin("Investment Growth Chart", 320, 240)
+    win = GraphWin("Investment Growth Chart", 640, 480)
+    win.setCoords(-1.75, -600, 11.5, 10400)
     win.setBackground("white")
-    Text(Point(20, 230), ' 0.0K').draw(win)
-    Text(Point(20, 180), ' 2.5K').draw(win)
-    Text(Point(20, 130), ' 5.0K').draw(win)
-    Text(Point(20, 80), ' 7.5K').draw(win)
-    Text(Point(20, 30), ' 10.0K').draw(win)
+    Text(Point(-1, 0), ' 0.0K').draw(win)
+    Text(Point(-1, 2500), ' 2.5K').draw(win)
+    Text(Point(-1, 5000), ' 5.0K').draw(win)
+    Text(Point(-1, 7500), ' 7.5K').draw(win)
+    Text(Point(-1, 10000), ' 10.0K').draw(win)
+    Text(Point(5, -400),'Years (1-10)').draw(win)
 
     #Draw bar for intial principal
     height = principal * 0.02
-    bar = Rectangle(Point(40, 230), Point(65, 230-height))
+    bar = Rectangle(Point(0, 0), Point(1, principal))
     bar.setFill("red")
     bar.setWidth(2)
     bar.draw(win)
@@ -30,12 +32,11 @@ def main():
     for year in range (1,11):
         principal = principal * (1 + apr)
         #draw bar for this value
-        x11 = year * 25 + 40
-        height = principal * 0.02
-        bar = Rectangle(Point(x11, 230), Point(x11+25, 230-height))
+        bar = Rectangle(Point(year, 0), Point(year+1, principal))
         bar.setFill("red")
-        bar.setWidth(2)
+        bar.setWidth(5)
         bar.draw(win)
+        bar.setOutline("black")
 
     input("Press <Enter> to quit")
     win.close()
